9290cfb6bc1008ba7b2ed80bd7baee4187372c58
TharukiJ <tharuki.fbacc@gmail.com>
2026-05-09 01:29:34 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -201,6 +201,33 @@ export default function NotionCalendarClone() {
             </motion.div>
 
           </div>
+
+          {/* Gooey Blob CTA */}
+          <div className="mt-20 flex justify-end">
+            <Link href="/login?step=role" className="buttons">
+              <button className="blob-btn">
+                Book Your Service
+                <span className="blob-btn__inner">
+                  <span className="blob-btn__blobs">
+                    <span className="blob-btn__blob"></span>
+                    <span className="blob-btn__blob"></span>
+                    <span className="blob-btn__blob"></span>
+                    <span className="blob-btn__blob"></span>
+                  </span>
+                </span>
+              </button>
+            </Link>
+
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className="hidden">
+              <defs>
+                <filter id="goo">
+                  <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+                  <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+                </filter>
+              </defs>
+            </svg>
+          </div>
         </div>
       </section>
 
